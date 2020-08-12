4699d95fdc3bed262f2e7fbfc976128fe51d8088
Vicente <vicente.cristi@outlook.com>
2020-08-12 18:32:36 -0400

    creo inventario json desde dict python

---
+++ Inventario/dict_inventario.py
@@ -0,0 +1,21 @@
+import json
+
+prod_higiene = { 'pasta_dientes': {
+    'cantidad' : "",
+    'pt_critico' : '3',
+    'pedir' : False},
+'papel_confort' : {
+    'cantidad' : "",
+    'pt_critico' : '6',
+    'pedir' : False},
+'jabon' : {
+    'cantidad' : "",
+    'pt_critico' : '3',
+    'pedir' : False},
+'shampoo' : {'cantidad' : "",
+    'pt_critico' : '3',
+    'pedir' : False},
+}
+
+with open('inventory.json', 'w') as json_file:
+    json.dump(prod_higiene, json_file)
--- Inventario/inventario.py
+++ Inventario/inventario.py
@@ -1,6 +1,2 @@
 import json
 
-with open ('inventario/listado.json', 'r+') as l:
-    listado = json.loads(l.read())
-
-print(type(listado))

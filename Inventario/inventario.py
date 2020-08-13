b0a0a0f710f8571a7a1ccacde56f0311d4bfaa09
Vicente <vicente.cristi@outlook.com>
2020-08-13 17:54:48 -0400

    habra esperanza?

--- Inventario/inventario.py
+++ Inventario/inventario.py
@@ -5,11 +5,16 @@ with open('inventario\inventory.json', 'r+') as f:
     inv_productos = json.load(f)
 
 opcion = str(input("Elegir un Producto "))
-
-productos = ""
-cantidad = ""
+producto = ""
+cantidad = "3"
 for i in inv_productos.keys():
-    productos = i
-    if opcion.lower() == productos:
-        print(productos)
+    producto = i
+    if opcion.lower() == producto:
+        print(f"existen {cantidad} unidades de {producto}")
+        want_input = input("Desea ingresar nueva cantidad? ")
+        if want_input.upper() == "SI":
+            new_q = int(input("Ingrese nueva cantidad "))
+            print(f"La cantidad de {producto} ha sido actualizada a {new_q}")
+        else:pass
     else: pass
+

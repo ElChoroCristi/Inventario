97a7b0230ceda9e770761277cbbc89da68a7e1ea
Vicente <vicente.cristi@outlook.com>
2020-08-12 23:11:58 -0400

    probando formas de llamar al diccionario

--- Inventario/dict_inventario.py
+++ Inventario/dict_inventario.py
@@ -1,18 +1,23 @@
 import json
 #declaro diccionarios de cada producto
 higiene = {'prod_higiene' : { 'pasta_dientes': {
+    'nombre' : 'pasta_dientes',
     'cantidad' : "",
     'pt_critico' : '3',
     'pedir' : False},
 'papel_confort' : {
+    'nombre' : 'papel_confort',
     'cantidad' : "",
     'pt_critico' : '6',
     'pedir' : False},
 'jabon' : {
+    'nombre' : 'jabon',
     'cantidad' : "",
     'pt_critico' : '3',
     'pedir' : False},
-'shampoo' : {'cantidad' : "",
+'shampoo' : {
+    'nombre' : 'shampoo',
+    'cantidad' : "",
     'pt_critico' : '3',
     'pedir' : False},
 }}
--- Inventario/inventario.py
+++ Inventario/inventario.py
@@ -3,3 +3,9 @@ import json
 #ingreso productos de .json
 with open('inventario\inventory.json', 'r+') as f:
     inv_productos = json.load(f)
+
+#inv_productos['prod_higiene']["jabon"]["cantidad"] = int(input(f'Ingrese Cantidad de {inv_productos["prod_higiene"]["jabon"]["nombre"]} - '))
+#print(inv_productos["prod_higiene"]["jabon"])
+
+for n in inv_productos['prod_higiene']:
+    print(type(n))
